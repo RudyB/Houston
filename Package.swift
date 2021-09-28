c775cd55b087b71c6304cfa22442ee8dadf21b73
Rudy Bermudez <hello@rudybermudez.io>
2021-09-27 22:54:57 -0700

    Remote un-necessary plists

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Houston",
+    platforms: [
+            .macOS(.v10_10), .iOS(.v9), .tvOS(.v9)
+        ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
